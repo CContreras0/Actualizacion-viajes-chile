98b70b7bc07e142758f98ed03791bbe2a7e29367
Claudia <cn.contrerasa@gmail.com>
2024-08-05 21:50:34 -0400

    Se subió modal en la parte de términos y condiciones del formulario, se hicieron algunos ajustes de la foto en quienes somos, se quitó margin left y right del body para agregarlo por cada sección

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,13 @@ import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 //
 // Place any custom JS here
 //
+
+$(document).ready(function () {
+    console.log("ready!");
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    // Se aplica posicionamiento top por defecto a todos los tooltips de la página
+    $('[data-toggle="tooltip"]').tooltip({
+        placement: 'top'
+    });
+});
